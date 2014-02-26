aa4f05efed551fd4ea4a540471b73deacd524c30
Chris Halbersma <chalbersma.12@gmail.com>
2014-02-26 09:37:31 -0600

    Update Proxy to include Depth from API
    nt

--- server/796rsmprxy.py
+++ server/796rsmprxy.py
@@ -13,6 +13,9 @@ def main() :
     # Market Trades URL
     tradeurl = "http://api.796.com/v3/stock/trades.html?type=rsm"
     
+    # Depth URL
+    depthurl = "http://api.796.com/v3/stock/depth.html?type=rsm"
+    
     ## Grab info For Ticker
     tickrequest = urllib.request.Request(tickurl)
     tickresponse = urllib.request.urlopen(tickrequest)
@@ -23,9 +26,15 @@ def main() :
     traderesponse = urllib.request.urlopen(traderequest)
     tradexjson = json.loads((traderesponse.read().decode('utf-8')))
     
+    ## Grab Info For Depth
+    depthrequest = urllib.request.Request(depthurl)
+    depthresponse = urllib.request.urlopen(depthrequest)
+    depthxjson = json.loads((depthresponse.read().decode('utf-8')))
+    
     ## Creat String with JS Files
     tickproxyfile = "/var/api/ticker.js"
     tradeproxyfile = "/var/api/trades.js"
+    depthproxyfile = "/var/api/depth.js"
     
     ## Create static ticker Info
     tickbegin = '''
@@ -50,20 +59,33 @@ def main() :
             }
             '''
     
+    ## Create Static Depth Info
+    depthbegin = '''
+    function depth(){
+    var depthstring = "'''
+    			
+    depthend = '''
+    	
+    return depthstring;
+    }
+    '''
             
     ## Ticker and Trade
     ticker_string = tickbegin + str(tickxjson['ticker']) + "\"" + tickend;
     trades_string = tradebegin + str(tradexjson) + "\"" + tradeend;
+    depth_string = depthbegin + str(depthxjson) + "\"" + depthend;
     
     ## Replace ' with " to meet JSON Lint Requirements
     ticker_string = ticker_string.replace("\'", "\\\"");
     trades_string = trades_string.replace("\'", "\\\"");
+    depth_string = depth_string.replace("\'", "\\\"");
     
    
     ## Log Ticker & Trade
     print ("Last Run")
     print (ticker_string)
     print (trades_string)
+    print (depth_string)
     
     ## Write Ticker File
     tickfile = open(tickproxyfile, 'w')
@@ -74,6 +96,11 @@ def main() :
     tradefile = open(tradeproxyfile, 'w')
     tradefile.write(trades_string)
     tradefile.close()
+    
+    ## Write Depth File
+    depthfile = open(depthproxyfile, 'w')
+    depthfile.write(depth_string)
+    depthfile.close()
     
     ## Log Writing
     print ("Wrote Files")
