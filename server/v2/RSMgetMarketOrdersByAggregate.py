14668fe5ca49a539c0f0acc74cc5fe2686e89ec2
chalbersma <chalbersma.12@gmail.com>
2014-06-17 17:16:05 +0000

    More work on Orders by Aggregate

--- server/v2/RSMgetMarketOrdersByAggregate.py
+++ server/v2/RSMgetMarketOrdersByAggregate.py
@@ -5,19 +5,30 @@
 # MarketOrdersByAggegate
 # ["LowOfInterval", "AskVolumeinShares", ""BuyVolumeinShares", "AskVolumeinmBTC", "BuyVolumeinmBTC"]
 
+import math
+
 def RSMgetMarketOrdersByAggegate(depthsdata, tickerdata):
   
-  # Hardcoded Interval of 0.5 mBTC
-  INTERVAL = 0.05
-  # Market Orders By Aggregate
-  LastPrice = float(tickerdata['ticker']['last']) / 0.001;
+  # Hardcoded Interval of 0.1 mBTC or 10,000 Satoshis
+  SINTERVAL = 1000
+  # Market Orders By Aggregate in Satoshis
+  SLastPrice = int(float(tickerdata['ticker']['last']) / 0.0000001);
+  # Multiplier Default Multiplier of 5
+  Multiplier = 10
+  while (Multiplier > 0 ):
+    SproposedLow = SLastPrice - (SINTERVAL * Multiplier)
+    if (SproposedLow < 0 ):
+      # Multiplier is too large
+      Multiplier = Multiplier - 1
+    else:
+      # Multiplier is good
+      break
   
-  print("Depths Data:")
-  print(depthsdata)
-  
-  print ("Price  "+  str(LastPrice))
-  minimum = LastPrice - INTERVAL * 10 
-  print(minimum)
+  # End of Loop
+  SLow = SproposedLow
+  # Begin Loop to find Bits
+  for i in range(-Multiplier, Multiplier):
+    print ("Range " + str(i) + " : " + str(SLastPrice + (SINTERVAL * i) ) + " to " + str(SLastPrice + (SINTERVAL * (i+1) )))
   
   
   
