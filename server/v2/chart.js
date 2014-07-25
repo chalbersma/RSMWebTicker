8bd95508f58188538e26e3f5d469fc61b18a43d6
Chris Halbersma <Chris.Halbersma@cerner.com>
2014-07-25 22:13:35 +0000

    Adding FE Javascript Stuff.

--- server/v2/chart.js
+++ server/v2/chart.js
@@ -4,6 +4,10 @@
 
 // Change This to Server that hosts js stuff
 HOSTSTRING="http://localhost:8080/RSMWebTicker/server/"
+function dummy(){
+	// Dummy Function
+	return
+}
 
 function populate(){
   // Google Scirpts
@@ -12,21 +16,29 @@ function populate(){
   });
 
   // Write Ticker
-  $.getScript(HOSTSTRING+'v2/js/Ticker.js', function(){
-    alert( "success" );
+/*  $.getScript(HOSTSTRING+'v2/js/Ticker.js', function(){
   })
     .done(function() {
       console.log("Successfully Loaded Ticker.js");
+			writeTicker();
     })
     .fail(function() {
-      alert( "error")
+      console.log("Loading Script Ticker.js Failed");
 			if (arguments[0].readyState==0){
-				alert ( "Failure to Load")
+				console.log("Network Issue: Can't reach&Load Ticker.js");
 			} else { 
-				alert (arguments[2].toString());
+				console.log("Unable to Parse Ticker.js. Parser error Message");
+				console.log((arguments[2].toString()));
 			}	
     })
     .always(function() {
       alert( "finished" )
     });
+*/
+
+	// Load Function for Writing the Ticker
+	LoadJSFile(HOSTSTRING, "v2/js/CurrentTicker.js", "CurrentTicker.js", function(){
+		LoadJSFile(HOSTSTRING, "v2/js/Ticker.js", "Ticker.js", function(){ writeCurrentTicker(CurrentTicker()) })
+	});
+
 }
--- server/v2/js/Ticker.js
+++ server/v2/js/Ticker.js
@@ -1,34 +1,19 @@
 /* Ticker.js */
 
-// Writes A Ticker in A Table
+function writeCurrentTicker(lastCurrent){
+	console.log(lastCurrent);
+	// Convert from String to JSON
+	lastCurrentActual = jQuery.parseJSON(lastCurrent);
+	// Hardcoded
+	TICKERDESCRIPTION = [ "Price", "TODAY Weighted Price" ,  "TODAY HR Hight", "TODAY HR Low", "TODAY HR VOL (SHARES)", "TODAY HR VOL (mBTC)" ];
+	var HTMLBITS = new Array();
 
-// Get Current Ticker
+	for (var i =0 ; i < lastCurrentActual.length; i++){
+		HTMLBITS[String(TICKERDESCRIPTION[i])] = "<tr><td> " + String(TICKERDESCRIPTION[i]) + " </td><td> " + String(lastCurrentActual[i]) + " </td></tr> " ;
+	}
+	
+	console.log(HTMLBITS);
 
-function writeTicker(){
-  console.log("In writeTicker");
-
-  TICKERDESCRIPTION = [ "Price", "TODAY Weighted Price" , "TODAY HR Hight", "TODAY HR Low", "TODAY HR VOL (SHARES)", "TODAY HR VOL (mBTC)" ];
-
-//  $.getScript('v2/js/CurrentTicker.js', function(){
-//    console.log("Sucessfully Loaded Current Ticker");
-//   });
-
-  lastCurrentTicker = CurrentTicker();
-  
-  /*
-  * [ "Price", "TODAY Weighted Price" "TODAY HR Hight", "TODAY HR Low", TODAY HR VOL (SHARES), "TODAY HR VOL (mBTC)" ]
-  * 
-  */
-  
-  // Generate HTML
-  HTML = new Array();
-  
-  for (var i = 0; i < TICKERDESCRIPTION.length(); i++ ){
-    HTML[TICKERDESCRIPTION[i]] = lastCurrentTicker[i];
-  }
-  document.writeln(HTML);
-  console.log(HTML);
-  
-
+	return
 }
 
