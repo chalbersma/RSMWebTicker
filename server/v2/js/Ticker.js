b5449e207c413210020da17347268ae5ca683692
Chris Halbersma <chalbersma.12@gmail.com>
2014-07-25 14:07:53 -0500

    Adding Ticker Fo' reals

---
+++ server/v2/js/Ticker.js
@@ -0,0 +1,9 @@
+/* Ticker.js */
+
+// Writes A Ticker in A Table
+
+// Get Current Ticker
+$.getScript('v2/js/CurrentTicker.js', function(){
+  console.log("Sucessfully Loaded Google Vizualization");
+});
+
