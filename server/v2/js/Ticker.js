cd2cb8bebb7f2e407a13301f871c3b7fe7897e2a
Chris Halbersma <Chris.Halbersma@cerner.com>
2014-07-26 01:53:54 +0000

    Adding Ticker Table & Graph Controls

--- server/v2/js/Ticker.js
+++ server/v2/js/Ticker.js
@@ -5,14 +5,23 @@ function writeCurrentTicker(lastCurrent){
 	// Convert from String to JSON
 	lastCurrentActual = jQuery.parseJSON(lastCurrent);
 	// Hardcoded
-	TICKERDESCRIPTION = [ "Price", "TODAY Weighted Price" ,  "TODAY HR Hight", "TODAY HR Low", "TODAY HR VOL (SHARES)", "TODAY HR VOL (mBTC)" ];
+	TICKERDESCRIPTION = [ "Price", "TODAY Weighted Price" ,  "TODAY HR High", "TODAY HR Low", "TODAY HR VOL (SHARES)", "TODAY HR VOL (mBTC)" ];
 	var HTMLBITS = new Array();
 
 	for (var i =0 ; i < lastCurrentActual.length; i++){
 		HTMLBITS[String(TICKERDESCRIPTION[i])] = "<tr><td> " + String(TICKERDESCRIPTION[i]) + " </td><td> " + String(lastCurrentActual[i]) + " </td></tr> " ;
 	}
 	
-	console.log(HTMLBITS);
+	console.log(String(HTMLBITS));
+	console.log("Writing to div tickbox")
+	htmlToPlace = "<table>";
+	for (var j =0 ; j < TICKERDESCRIPTION.length ; j++){
+		console.log(j);
+		htmlToPlace += HTMLBITS[String(TICKERDESCRIPTION[j])];
+		console.log(htmlToPlace)
+	}
+	htmlToPlace += "</table>";
+	document.getElementById('tickbox').innerHTML = htmlToPlace;
 
 	return
 }
